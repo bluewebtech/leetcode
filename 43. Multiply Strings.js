762e7a46fdeb6b2b1f39613d282525b0e906887b
Peter Morrison <bwt.bluewebtech@gmail.com>
2020-05-13 06:25:43 -0400

    43. Multiply Strings

---
+++ 43. Multiply Strings.js
@@ -0,0 +1,10 @@
+/**
+ * The quick one liner solution.
+ *
+ * @param {String} num1
+ * @param {String} num2
+ * @return {String}
+ */
+const multiply = (num1, num2) => {
+  return (Number(num1) * Number(num2)).toString();
+};
